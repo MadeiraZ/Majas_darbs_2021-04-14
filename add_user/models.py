75acd6e19de14d2944f0a50506b454d7808bb61a
Madara <madara.safonova@gmail.com>
2021-04-26 20:54:55 +0300

    Change username max_lenght to 50

--- add_user/models.py
+++ add_user/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class AddUser(models.Model):
 
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
